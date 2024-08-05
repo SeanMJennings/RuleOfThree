85b14302939955489b538f44ce37c9c290e1952d
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-08-05 19:33:19 +0100

    Fix alter settings

--- server/alter_alembic_ini.py
+++ server/alter_alembic_ini.py
@@ -2,11 +2,9 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read("alembic.ini")
-config.set(
-    "sqlalchemy.url",
-    "value",
-    "mssql://sa:YourStrong@Passw0rdFakeForSourceControl@localhost/ruleofthree",
-)
+config["alembic"][
+    "sqlalchemy.url"
+] = "mssql://sa:YourStrong@Passw0rdFakeForSourceControl@localhost/ruleofthree"
 
 with open("alembic.ini", "w") as configfile:
     config.write(configfile)

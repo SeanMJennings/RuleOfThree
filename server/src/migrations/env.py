0032fdb03d42fac14907bdbb2a3aebcff996337a
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-08-06 10:51:12 +0100

    Fix alter settings

--- server/src/migrations/env.py
+++ server/src/migrations/env.py
@@ -6,6 +6,7 @@ from sqlalchemy_utils import create_database
 import re
 from alembic import context
 from pathlib import Path
+from sqlalchemy.engine import URL
 import yaml
 
 secret_config_path = Path(__file__).parent / "../../secret_config.yaml"
@@ -36,7 +37,13 @@ def create_db_if_not_exists():
     db_uri = config.get_section(config.config_ini_section, {})["sqlalchemy.url"]
     database = re.search(r"^(?P<dbname>[^?]+)", db_uri.split("/")[-1]).group("dbname")
     try:
-        print(f"DB uri: {db_uri}")
+        print("start of alternate test")
+        connection_string = "DRIVER=ODBC Driver 17 for SQL Server;SERVER=localhost;UID=sa;PWD=YourStrong@Passw0rdFakeForSourceControl"
+        connection_url = URL.create(
+            "mssql+pyodbc", query={"odbc_connect": connection_string}
+        )
+        engine = create_engine(connection_url)
+        print("end of alternate test")
         engine = create_engine(db_uri)
         with engine.connect():
             print(f"Database {database} already exists.")

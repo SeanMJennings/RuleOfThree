c0d37deeeaba48d0b10511cdc2b69179059c95ca
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-08-06 16:08:03 +0100

    Fix migration

--- server/alter_alembic_ini.py
+++ server/alter_alembic_ini.py
@@ -1,13 +1,10 @@
 import configparser
-from sqlalchemy.engine import URL
 
 config = configparser.ConfigParser()
 config.read("alembic.ini")
-master_connection_string = "DRIVER=ODBC Driver 17 for SQL Server;SERVER=localhost;DATABASE=ruleofthree;UID=sa;PWD=YourStrong@Passw0rdFakeForSourceControl"
-master_connection_url = URL.create(
-    "mssql+pyodbc", query={"odbc_connect": master_connection_string}
-)
-config["alembic"]["sqlalchemy.url"] = master_connection_url
+config["alembic"][
+    "sqlalchemy.url"
+] = "mssql+pyodbc://?odbc_connect=DRIVER%3DODBC+Driver+17+for+SQL+Server%3BSERVER%3Dlocalhost%3BUID%3Dsa%3BPWD%3DYourStrong%40Passw0rdFakeForSourceControl"
 
 with open("alembic.ini", "w") as configfile:
     config.write(configfile)

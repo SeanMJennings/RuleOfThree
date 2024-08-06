3b606e49066edede5bc327b7de50864cddf7fcdc
Sean Jennings <59314025+SeanMJennings@users.noreply.github.com>
2024-08-06 10:25:21 +0100

    Fix alter settings

--- server/alter_alembic_ini.py
+++ server/alter_alembic_ini.py
@@ -4,7 +4,7 @@ config = configparser.ConfigParser()
 config.read("alembic.ini")
 config["alembic"][
     "sqlalchemy.url"
-] = "mssql+pyodbc://sa:YourStrong@Passw0rdFakeForSourceControl@localhost/ruleofthree?driver=ODBC Driver 17 for SQL Server"
+] = "mssql+pyodbc://sa:YourStrong@Passw0rdFakeForSourceControl@localhost:1433/ruleofthree?driver=ODBC Driver 17 for SQL Server"
 
 with open("alembic.ini", "w") as configfile:
     config.write(configfile)
